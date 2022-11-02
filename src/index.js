e17397dc124a4fefbab55702a1c974ef9ed13e89
Ayush Kumar Dubey <ku.ayush@yahoo.com>
2022-11-02 05:26:03 +0000

    Update 13 files

--- src/index.js
+++ src/index.js
@@ -8,3 +8,14 @@ document.getElementById("app").innerHTML = `
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
+checkPalindrome("Ayush");
+
+function checkPalindrome(string) {
+  const len = string.length;
+  for (let i = 0; i < len / 2; i++) {
+    if (string[i] !== string[len - 1 - i]) {
+      return "Not a palindrome";
+    }
+  }
+  return "is palindrome";
+}
